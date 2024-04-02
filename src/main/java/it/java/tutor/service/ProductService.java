9999acbc19ab338ff697e334fd4a7541860986fe
Sil-Cro-Dev <s.paloni@solvingteam.it>
2024-04-02 19:25:47 +0200

    fix imports

--- src/main/java/it/java/tutor/service/ProductService.java
+++ src/main/java/it/java/tutor/service/ProductService.java
@@ -2,9 +2,6 @@ package it.java.tutor.service;
 
 import it.java.tutor.dto.ProductDto;
 import it.java.tutor.repository.ProductRepository;
-import lombok.extern.log4j.Log4j;
-import lombok.extern.log4j.Log4j2;
-import org.hibernate.PropertyValueException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
